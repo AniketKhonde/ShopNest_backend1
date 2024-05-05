14b349c87562b5b3832b96d88c92a34f6a168245
AniketKhonde <aniketkhonde51@gmail.com>
2024-05-05 10:36:01 +0530

    vercel.json

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -26,7 +26,6 @@ const loginUser = async (req, res) => {
     const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h' // Token expires in 1 hour
     });
-     console.log("token>>>>>>>>>>", accessToken)
     // Send the access token in the response
     res.status(200).json({ message: 'Login successful', jwtToken:accessToken, userId:user._id  });
   } catch (error) {
